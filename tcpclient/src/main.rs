87dc03e80356f8ec08a359a09d397c62579b0642
xero7689 <volleyp7689@gmail.com>
2025-06-06 22:07:08 +0800

    feat: Add basic tcp server

---
+++ tcpclient/src/main.rs
@@ -0,0 +1,11 @@
+use std::io::{Read, Write};
+use std::net::TcpStream;
+use std::str;
+
+fn main() {
+    let mut _stream = TcpStream::connect("localhost:3000").unwrap();
+    _stream.write("Hello server!".as_bytes()).unwrap();
+    let mut buffer = [0; 5];
+    _stream.read(&mut buffer).unwrap();
+    println!("Got response from server: {:?}", str::from_utf8(&buffer).unwrap());
+}
