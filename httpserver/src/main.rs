78fda50baacd2d3cdfb57b2ebec7399d5ade93e4
xero7689 <volleyp7689@gmail.com>
2025-06-14 19:15:46 +0800

    fix: Add template for deploy function to Lambda

--- httpserver/src/main.rs
+++ httpserver/src/main.rs
@@ -5,7 +5,7 @@ use server::Server;
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let server = Server::new("localhost:3000");
+    let server = Server::new("127.0.0.1:8080");
     server.run().await?;
     Ok(())
 }
